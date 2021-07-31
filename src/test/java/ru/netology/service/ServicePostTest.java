468a926c95290a7ac860bb4a7b2d22a400ae150d
Zosha <posyamyka@gmail.com>
2021-07-31 15:57:02 +0300

    Success

--- src/test/java/ru/netology/service/ServicePostTest.java
+++ src/test/java/ru/netology/service/ServicePostTest.java
@@ -11,14 +11,14 @@ public class ServicePostTest {
 
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .body("some data")
+
                 .when()
                 .post("/post")
-// Проверки
+
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
