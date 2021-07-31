100b4ee6eaa66a7352b8f474dfb131243c5f47a9
Zosha <posyamyka@gmail.com>
2021-07-31 15:54:04 +0300

    fail

--- src/test/java/ru/netology/service/ServicePostTest.java
+++ src/test/java/ru/netology/service/ServicePostTest.java
@@ -18,7 +18,7 @@ public class ServicePostTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
