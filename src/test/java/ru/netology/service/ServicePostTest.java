2bca089d56606ed759f534a334a2291b1af2d32b
Zosha <posyamyka@gmail.com>
2021-07-31 15:47:45 +0300

    commit2

---
+++ src/test/java/ru/netology/service/ServicePostTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class ServicePostTest {
+    @Test
+    void post() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
